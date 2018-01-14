58dcb9d07def5805350cd818a50c70ad8f086888
Mark Smith <smith.h.mark@gmail.com>
2018-01-13 16:24:43 -0800

    Added initial Grid object
    Grid is intended to hold the state of what we know to be true about the
    state of the game.

--- Grid.py
+++ Grid.py
@@ -1,3 +1,29 @@
 
 grid = "grid"
 
+class Grid():
+    def __init__(self, symbolcnt=9, board=None):
+        self._symbols = {x for x in range(symbolcnt)}
+        self._symbolcnt = symbolcnt
+        if board is None:
+            self._board = [None for unused in range(symbolcnt**2)]
+        else:
+            allowed = set()
+            allowed.add(None)
+            allowed.update(self._symbols)
+            #allowed = {None}.update(self._symbols)
+
+            for ii, pos in enumerate(board):
+                if pos not in allowed:
+                    raise ValueError("board cannot contain symbols outside"+ repr(allowed))
+            if all(map(lambda pos: pos in allowed, board)):
+              #and not any(map(lambda pos: pos not in allowed, board)):
+                self._board = board[:]
+
+            else:
+                raise ValueError("board must be square, symbolcnt on a side, and not contain any symbols outside of the allowed range")
+
+    def _offset(self, x, y):
+        return y * self._symbolcnt + x
+    def at(self, x, y):
+        return self._board[self._offset(x, y)]
--- test_Grid.py
+++ test_Grid.py
@@ -1,5 +1,40 @@
+import pytest
+from unittest.mock import Mock
+
 import Grid
 
-def test_Grid():
+def test_grid():
     assert Grid.grid == "grid"
+
+@pytest.fixture
+def empty_grid():
+    return Grid.Grid()
+
+def test_standard_constructor(empty_grid):
+    assert len(empty_grid._symbols) == 9
+    assert empty_grid._symbols == {x for x in range(9)}
+    assert len(empty_grid._board) == 9 * 9
+    for pos in empty_grid._board:
+        assert pos is None
+
+def test_offset_calculation_sz2():
+    g = Grid.Grid()
+    g._symbolcnt = 2
+    g._board = list(range(4))
+
+    assert g._offset(0,0) == 0
+    assert g.at(0,0) == 0
+    assert g._offset(1,0) == 1
+    assert g.at(1,0) == 1
+    assert g._offset(0,1) == 2
+    assert g.at(0,1) == 2
+    assert g._offset(1,1) == 3
+    assert g.at(1,1) == 3
+
+def test_sz2_constructort():
+    board = [0,1,1,0]
+    g = Grid.Grid(2, board)
+
+    assert g._symbolcnt == 2
+    assert g._board == board
 
