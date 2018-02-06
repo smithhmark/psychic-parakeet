620387a158d6f36ccd60c7d318aa1c4fe0aa9202
Mark Smith <smith.h.mark@gmail.com>
2018-02-06 10:11:05 -0800

    adding hard puzzle fixture

--- test_Grid.py
+++ test_Grid.py
@@ -77,6 +77,45 @@ def simple_puzzle_answer():
     return list(map(lambda k: k-1, rawdata))
 
 @pytest.fixture
+def hard_puzzle():
+    """provides "Vegard Hanssen puzzle 2155141"
+    """
+    rawdata = [
+        None,None,None, 6,None,None, 4,None,None,
+        7,None,None, None,None,3, 6,None,None,
+        None,None,None, None,9,1, None,8,None,
+
+        None,None,None,None,None,None,None,None,None,
+        None,5,None, 1,8,None, None,None,3,
+        None,None,None, 3,None,6, None,4,5,
+
+        None,4,None, 2,None,None, None,6,None,
+        9,None,3, None,None,None, None,None,None,
+        None,2,None, None,None,None, 1,None,None]
+    data = []
+    for ii in rawdata:
+        if ii is None:
+            data.append(ii)
+        else:
+            data.append(ii - 1)
+    return data
+
+def hard_puzzle_answer():
+    rawdata = [
+        5,8,1, 6,7,2, 4,3,9,
+        7,9,2, 8,4,3, 6,5,1,
+        3,6,4, 5,9,1, 7,8,2,
+
+        4,3,8, 9,5,7, 2,1,6,
+        2,5,6, 1,8,4, 9,7,3,
+        1,7,9, 3,2,6, 8,4,5,
+
+        8,4,5, 2,1,9, 3,6,7,
+        9,1,3, 7,6,8, 5,2,4,
+        6,2,7, 4,3,5, 1,9,8]
+    return list(map(lambda k:k-1, rawdata))
+
+@pytest.fixture
 def simple_grid_puzzle(simple_puzzle):
     return Grid.Grid(9,simple_puzzle)
 
